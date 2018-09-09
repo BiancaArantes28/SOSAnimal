5d2868a16ab5faaba0792c520b30b779e5d53b66
Andre Goncalves <andre.vgoncalves@hotmail.com>
2018-09-09 20:06:33 -0300

    elementos criados na tela

---
+++ app/src/main/java/com/uninove/homepets/ui/CadastroOngActivity.java
@@ -0,0 +1,15 @@
+package com.uninove.homepets.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.uninove.homepets.R;
+
+public class CadastroOngActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_ong);
+    }
+}
